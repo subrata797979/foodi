eaab688ed1260dc2ffa7236d1b2cf468021e3882
subrata797979 <subrata797979@gmail.com>
2022-06-30 07:50:07 +0530

    Initial setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import {MainComponent} from "./main/main.component";
+import {AboutComponent} from "./about/about.component";
+import {RoutingModule} from "./routing/routing.module";
+import { HeaderComponent } from './header/header.component';
+import {NgxHideOnScrollModule} from "ngx-hide-on-scroll";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AboutComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule,
+    NgxHideOnScrollModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
